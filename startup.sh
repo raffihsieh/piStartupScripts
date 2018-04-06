5a8979ea557c8abfda12e27b073fec9ceb6b77d8
Raphael Hsieh <raffihsieh@gmail.com>
2018-04-06 18:30:30 +0000

    better startup

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get dist-upgrade
 sudo apt-get install vim
 sudo apt-get install npm
 sudo apt-get install xclip 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
